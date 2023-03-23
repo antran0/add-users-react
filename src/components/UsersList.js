7644f79e82ee3774cd87ff06679343f4577931a5
Steven Tran <steven.tran550@gmail.com>
2023-03-22 21:20:36 -0700

    refactored implementation

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,13 +3,13 @@ import User from "./User";
 import styles from "./UsersList.module.css";
 
 const UserList = (props) => {
-  let usersToDisplay = [];
-
-  for (const user of props.users) {
-    usersToDisplay.push(<User key={user.id} name={user.name} age={user.age} />);
-  }
-
-  return <ul className={`${styles["users-list"]}`}>{usersToDisplay}</ul>;
+  return (
+    <ul className={`${styles["users-list"]}`}>
+      {props.users.map((user) => (
+        <User key={user.id} name={user.name} age={user.age} />
+      ))}
+    </ul>
+  );
 };
 
 export default UserList;
