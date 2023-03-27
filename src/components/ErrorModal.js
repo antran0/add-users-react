359dcf09f05c232d26b781edf1e06dc1d2e77e47
Steven Tran <steven.tran550@gmail.com>
2023-03-27 15:26:48 -0700

    added backdrop to error modal

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,18 +1,22 @@
 import React from "react";
 import styles from "./ErrorModal.module.css";
+import Backdrop from "./UI/Backdrop";
 import Card from "./UI/Card";
 
 const ErrorModal = (props) => {
   return (
-    <Card className={`${styles["error-modal"]}`}>
-      <section>
-        <h2>Invalid input</h2>
-        <p>{props.errorMessage}</p>
-        <button type="button" onClick={props.onCloseModal}>
-          Okay
-        </button>
-      </section>
-    </Card>
+    <div>
+      <Backdrop onClick={props.onCloseModal} />
+      <Card className={`${styles["error-modal"]}`}>
+        <section>
+          <h2>Invalid input</h2>
+          <p>{props.errorMessage}</p>
+          <button type="button" onClick={props.onCloseModal}>
+            Okay
+          </button>
+        </section>
+      </Card>
+    </div>
   );
 };
 
