9453835359fc406c4608a409dc72de5e5aeff390
Steven Tran <steven.tran550@gmail.com>
2023-03-28 20:54:35 -0700

    portaled error modal outside of app root

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import styles from "./ErrorModal.module.css";
 import Backdrop from "./UI/Backdrop";
 import Button from "./UI/Button";
@@ -7,16 +8,22 @@ import Card from "./UI/Card";
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
-      <Backdrop onClick={props.onCloseModal} />
-      <Card className={`${styles["error-modal"]}`}>
-        <section>
-          <h2>{props.title}</h2>
-          <p>{props.message}</p>
-          <Button type="button" onClick={props.onCloseModal}>
-            Okay
-          </Button>
-        </section>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onClick={props.onCloseModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Card className={`${styles["error-modal"]}`}>
+          <section>
+            <h2>{props.title}</h2>
+            <p>{props.message}</p>
+            <Button type="button" onClick={props.onCloseModal}>
+              Okay
+            </Button>
+          </section>
+        </Card>,
+        document.getElementById("overlay-root")
+      )}
     </React.Fragment>
   );
 };
